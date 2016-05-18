7aeafa628fea1863287c6e3551cc40df1d9aded9
ft <happiness_he@126.com>
2016-05-18 16:38:12 +0800

    master commit

--- src/hello/HelloWorldMaster.java
+++ src/hello/HelloWorldMaster.java
@@ -1,10 +1,10 @@
 package hello;
 
-public class HelloWorld {
+public class HelloWorldMaster {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world every one");
+
 	}
 
 }
