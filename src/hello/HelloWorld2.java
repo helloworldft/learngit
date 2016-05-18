bbedd8aef44a6ee192967e05ac4e386cd949146e
ft <happiness_he@126.com>
2016-05-18 14:31:40 +0800

    test

--- src/hello/HelloWorld2.java
+++ src/hello/HelloWorld2.java
@@ -4,7 +4,7 @@ public class HelloWorld2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("helloworld2");
+		System.out.println("helloworld2 every one +++");
 	}
 
 }
